282c9810a55810efed457abee32c396fc062f7ae
silumankadal <63130587+silumankadal@users.noreply.github.com>
2024-10-05 15:11:57 +0700

    v1

--- controllers/product.go
+++ controllers/product.go
@@ -39,37 +39,6 @@ func CreateProduct(c *gin.Context) {
 	c.JSON(http.StatusCreated, Product)
 }
 
-// func UpdateProduct(c *gin.Context) {
-// 	db := database.GetDB()
-// 	userData := c.MustGet("userData").(jwt.MapClaims)
-// 	contentType := helpers.GetContentType(c)
-
-// 	Product := models.Product{}
-// 	productId, _ := strconv.Atoi(c.Param("productId"))
-// 	userID := uint(userData["id"].(float64))
-
-// 	if contentType == appJSON {
-// 		c.ShouldBindJSON(&Product)
-// 	} else {
-// 		c.ShouldBind(&Product)
-// 	}
-
-// 	Product.UserID = userID
-// 	Product.ID = uint(productId)
-
-// 	err := db.Model(&Product).Where("id = ?", productId).Updates(models.Product{Title: Product.Title, Description: Product.Description}).Error
-
-// 	if err != nil {
-// 		c.JSON(http.StatusBadRequest, gin.H{
-// 			"error":   "Bad Request",
-// 			"message": err.Error(),
-// 		})
-// 		return
-// 	}
-
-// 	c.JSON(http.StatusOK, Product)
-// }
-
 func Topup(c *gin.Context) {
 	db := database.GetDB()
 	userData := c.MustGet("userData").(jwt.MapClaims)
